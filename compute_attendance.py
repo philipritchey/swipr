3067e258d1969720e78638c316e5cbccdbc948e9
pcr@CSE-RITCHEY-NB1 <pcr@tamu.edu>
2019-12-30 16:45:20 +0800

    refactor load_roster to reduce cognitive complexity

--- compute_attendance.py
+++ compute_attendance.py
@@ -3,29 +3,28 @@ from datetime import datetime
 def load_roster(filename):
   name = dict()
   line_no = 0
-  with open(filename) as f:
-    for line in f:
-      line_no += 1
-      try:
-        values = line.split()
-        uin = values[-1]
-        last = values[0]
-        first = ' '.join(values[1:-1])
-      except (ValueError, IndexError) as err:
-        print(err)
-        print('at line {}'.format(line_no))
-        break
-      if uin in name and not (first == name[uin]['first'] and last == name[uin]['last']):
-        print('[WARNING] {} already exists, replacing with new value'.format(uin))
-        print('  old: {} {}'.format(name[uin]['first'], name[uin]['last']))
-        print('  new: {} {}'.format(first, last))
-        #c = input('replace? (y/n) ')
-        #if c == 'n':
-        #    continue
-      else:
-        name[uin] = dict()
-      name[uin]['first'] = first;
-      name[uin]['last'] = last
+  for line in open(filename):
+    line_no += 1
+    #try:
+    values = line.split()
+    uin = values[-1]
+    last = values[0]
+    first = ' '.join(values[1:-1])
+    #except (ValueError, IndexError) as err:
+    #  print(err)
+    #  print('at line {}'.format(line_no))
+    #  break
+    if uin in name and (first != name[uin]['first'] or last != name[uin]['last']):
+      print('[WARNING] {} already exists, replacing with new value'.format(uin))
+      print('  old: {} {}'.format(name[uin]['first'], name[uin]['last']))
+      print('  new: {} {}'.format(first, last))
+      #c = input('replace? (y/n) ')
+      #if c == 'n':
+      #    continue
+    else:
+      name[uin] = dict()
+    name[uin]['first'] = first;
+    name[uin]['last'] = last
   return name
 
 def split_on_comma_or_whitespace(line):
@@ -59,7 +58,10 @@ def compute_attendance(name, attendance, filename='attendance.csv'):
   print('done.')
 
 def main():
-  name = load_roster('roster')
+  try:
+    name = load_roster('roster')
+  except (ValueError, IndexError) as err:
+    print("Error loading roster:",err)
   attendance = load_swipe_log('swipe_log')
   compute_attendance(name, attendance)
 
