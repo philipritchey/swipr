8fd96e57635e0d92b812c047d73fc9ab621cad4d
Philip Ritchey <pcr@tamu.edu>
2020-01-13 14:16:03 -0600

    refactored username and roster path, added sorting and duplicate removal

--- batch_mode_rename_images.sh
+++ batch_mode_rename_images.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+username="red"
+#username="instructor"
+
 # absolute path to image rosters
-pathToImageRosters="/home/instructor/Downloads/image_rosters"
+pathToImageRosters="/home/$username/Downloads/image_rosters"
 
 # prefix of image files, e.g. "BWXK_GET_PHOTO(1).display"
 photoPrefix="BWXK_GET_PHOTO"
 
+# absolute path to swipr/roster
+rosterFile="/home/$username/Documents/swipr/roster"
+
 # put your sections in the list {...}
 # you can do this for multiple classes, too
 #   e.g. {121_{513..520},431_{500..502}}
-for class in 121_{513..520}
+for class in {121_{521..528},489_502,713_600}
 do
     # setup paths arguments
     pathToPhotos="$pathToImageRosters/$class""_files"
@@ -25,8 +31,14 @@ do
     done
     
     # add names and UINs to swipr/roster file
+    echo "adding names and UINs for $class to $rosterFile"
     {
         cut -d, -s -f1,2,4 $pathToRoster | sed '1d' | sed 's/,/\t/g'
-    } >> /home/instructor/Documents/swipr/roster
+    } >> $rosterFile
 done
+
+# sort and remove duplicates from swipr/roster
+echo "sorting $rosterFile and removing duplicates"
+sort $rosterFile | uniq > $rosterFile
+
 echo "Done!"
